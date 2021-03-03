4ccbdf8d04d186fdda05c8473d6ff4ecc5d913ac
Jecesario <jp@soloid.com.br>
2021-03-03 17:57:40 -0300

    feat:(test): criando metodo de test salvarComTelNulo

---
+++ src/test/java/com/jecesario/testesunitarios/contato/ContatosRepositoryIntegrationTest.java
@@ -0,0 +1,42 @@
+package com.jecesario.testesunitarios.contato;
+
+import javax.validation.ConstraintViolationException;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.jecesario.testesunitarios.models.Contato;
+import com.jecesario.testesunitarios.repositories.ContatoRepository;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class ContatosRepositoryIntegrationTest {
+	
+	private Contato contato;
+	
+	@Autowired
+	private ContatoRepository contatoRepository;
+	
+	@Before
+	public void start() {
+		contato = new Contato("Gabriel", "011y", "9xxxxxxx9");
+	}
+	
+	@Rule
+	public ExpectedException esperadaExcecao = ExpectedException.none();
+	
+	@Test
+	public void salvarComTelNulo() throws Exception {
+		esperadaExcecao.expect(ConstraintViolationException.class);
+		esperadaExcecao.expectMessage("O telefonee deve ser preenchido");
+		
+		contato.setTelefone(null);
+		contatoRepository.save(contato);
+	}
+}
