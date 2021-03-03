a2c98cf84a10b61ab931f4082e7a0d81fd3c7778
Jecesario <jp@soloid.com.br>
2021-03-03 17:59:51 -0300

    feat:(test): criando metodo de test salvarComDddNulo

--- src/test/java/com/jecesario/testesunitarios/contato/ContatosRepositoryIntegrationTest.java
+++ src/test/java/com/jecesario/testesunitarios/contato/ContatosRepositoryIntegrationTest.java
@@ -17,26 +17,36 @@ import com.jecesario.testesunitarios.repositories.ContatoRepository;
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class ContatosRepositoryIntegrationTest {
-	
+
 	private Contato contato;
-	
+
 	@Autowired
 	private ContatoRepository contatoRepository;
-	
+
 	@Before
 	public void start() {
 		contato = new Contato("Gabriel", "011y", "9xxxxxxx9");
 	}
-	
+
 	@Rule
 	public ExpectedException esperadaExcecao = ExpectedException.none();
-	
+
 	@Test
 	public void salvarComTelNulo() throws Exception {
 		esperadaExcecao.expect(ConstraintViolationException.class);
 		esperadaExcecao.expectMessage("O telefonee deve ser preenchido");
-		
+
 		contato.setTelefone(null);
 		contatoRepository.save(contato);
 	}
+
+	@Test
+	public void salvarComDddNulo() throws Exception {
+		esperadaExcecao.expect(ConstraintViolationException.class);
+		esperadaExcecao.expectMessage("O DDD deve ser preenchido");
+
+		contato.setDdd(null);
+		contatoRepository.save(contato);
+	}
+
 }
