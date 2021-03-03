9a0b71ddece02499e87b9d50a99f14814a6ce2d3
Jecesario <jp@soloid.com.br>
2021-03-03 17:33:34 -0300

    chore(models): criando model contato

---
+++ src/main/java/com/jecesario/testesunitarios/models/Contato.java
@@ -0,0 +1,66 @@
+package com.jecesario.testesunitarios.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class Contato {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotEmpty(message = "O DDD deve ser preenchido")
+	private String ddd;
+
+	@NotEmpty(message = "O telefone deve ser preenchido")
+	private String telefone;
+
+	@NotEmpty(message = "O nome deve ser preenchido")
+	private String nome;
+
+	public Contato() {
+	}
+
+	public Contato(String ddd, String telefone, String nome) {
+		this.ddd = ddd;
+		this.telefone = telefone;
+		this.nome = nome;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDdd() {
+		return ddd;
+	}
+
+	public void setDdd(String ddd) {
+		this.ddd = ddd;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
